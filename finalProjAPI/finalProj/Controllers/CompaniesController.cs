816276aecefe765a13ecc05ee6c25be28971d878
dohamoamen <doha.moamen@gmail.com>
2018-05-31 00:23:05 +0200

    finalprojapi

---
+++ finalProjAPI/finalProj/Controllers/CompaniesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using finalProj.Models;
+
+namespace finalProj.Controllers
+{
+    public class CompaniesController : ApiController
+    {
+
+        [HttpGet]
+        public List<CompanyType> GetCompany()
+        {
+            using (DBDataModel db = new DBDataModel())
+            {
+                var companiesList = (from c in db.Companies
+                                     join s in db.Sectors on c.Sec_id equals s.Sec_id
+                                     join i in db.Industries on c.Ind_id equals i.Ind_id
+                                     select new CompanyType
+                                     {
+                                         Comp_id = c.Comp_id,
+                                         Comp_Name = c.Comp_Name,
+                                     //    Reu_Code = c.Reu_Code,
+                                         Com_Country = c.Com_Country,
+                                         Trd_Curr = c.Trd_Curr,
+                                         Curr_Out_Shares = (decimal)c.Curr_Out_Shares,
+                                         IMG_URL = c.IMG_URL,
+                                         Comp_Country = c.Com_Country,
+                                         Sec_Name = c.Sector.Sec_Name,
+                                         Ind_Name = c.Industry.Ind_Name
+                                     }).ToList();
+
+                return companiesList;
+
+
+            }
+        }
+        [HttpPost]
+        public CompanyType GetCompany(int id)
+        {
+            using (DBDataModel db = new DBDataModel())
+            {
+                var company = (from c in db.Companies
+                               join s in db.Sectors on c.Sec_id equals s.Sec_id
+                               join i in db.Industries on c.Ind_id equals i.Ind_id
+                               select new CompanyType
+                               {
+                                   Comp_id = c.Comp_id,
+                                   Comp_Name = c.Comp_Name,
+                                //   Reu_Code = c.Reu_Code,
+                                   Com_Country = c.Com_Country,
+                                   Trd_Curr = c.Trd_Curr,
+                                   Curr_Out_Shares = (decimal)c.Curr_Out_Shares,
+                                   IMG_URL = c.IMG_URL,
+                                   Comp_Country = c.Com_Country,
+                                   Sec_Name = c.Sector.Sec_Name,
+                                   Ind_Name = c.Industry.Ind_Name
+                               }).Where(p => p.Comp_id == id).FirstOrDefault();
+
+                return company;
+            }
+        }
+    }
+}
---
+++ finalProjAPI/finalProj/Controllers/OneYearBackwardController.cs
@@ -0,0 +1,34 @@
+﻿using finalProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+namespace finalProj.Controllers
+{
+    public class OneYearBackwardController : ApiController
+    {
+        // GET api/<controller>
+       
+
+        // GET api/<controller>/5
+        
+        public List<CompPrice> Get(int compid1)
+        {
+            using (DBDataModel db = new DBDataModel())
+            {
+                var x = DateTime.Now.AddMonths(-12);
+                var q = (from c in db.Companies
+                         join H in db.Historical_Price on c.Comp_id equals H.Comp_id
+                         where H.Date >= x && c.Comp_id == compid1
+                         select new CompPrice { Comp_Name = c.Comp_Name, Price_val = H.Price_val, }).ToList();
+                return q;
+            }
+        }
+      
+    }
+}
