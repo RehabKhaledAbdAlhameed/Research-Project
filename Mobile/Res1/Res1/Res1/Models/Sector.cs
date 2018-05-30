199993d443a85d56ca2a7e77e49610a5bca468a5
mohamedfarid9454 <mohamedgalal9454@gmail.com>
2018-05-30 14:47:47 +0200

    Xamarin_V1

---
+++ Mobile/Res1/Res1/Res1/Models/Sector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Res1.Models
+{
+    public class Sector
+    {
+        public int Sec_id { get; set; }
+
+    
+        public string Sec_Name { get; set; }
+
+        public string IMG_URL { get; set; }
+
+        public string Sec_Desc { get; set; }
+    }
+}
---
+++ Mobile/Res1/Res1/Res1/Services/IndustryServices.cs
@@ -0,0 +1,27 @@
+﻿using Res1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Res1.Services
+{
+    public class IndustryServices
+    {
+
+        public static List<Industry> GetIndustries()
+        {
+            var result = new List<Industry>() {
+
+               new Industry(){Ind_Name="I1",Ind_id=1, Sec_id=1,IMG_URL=""},
+               new Industry(){Ind_Name="I2",Ind_id=2, Sec_id=1,IMG_URL=""},
+               new Industry(){Ind_Name="I3",Ind_id=3, Sec_id=2,IMG_URL=""},
+               new Industry(){Ind_Name="I4",Ind_id=4, Sec_id=3,IMG_URL=""},
+               new Industry(){Ind_Name="I5",Ind_id=5, Sec_id=2,IMG_URL=""},
+
+
+
+            };
+            return result;
+        }
+    }
+}
